d3ad781b6ffbca730cd93b20fdf76c5713980c5b
Balwant223 <balwantdod223@gmail.com>
2021-08-21 14:28:40 +0530

    Project Model ,Form and Urls Added

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Project(models.Model):
+    name=models.CharField(max_length=20,null=False)
+    start=models.DateField()
+    end=models.DateField()
+    value=models.DecimalField(max_digits=50,decimal_places=20)
